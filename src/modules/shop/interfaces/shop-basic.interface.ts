9e508275cd145f730e1d9ff754bfda50c294774a
hpns47 <dani.abd.20069@gmail.com>
2026-04-30 20:17:21 +0500

    fix: added name return to shop service findById

---
+++ src/modules/shop/interfaces/shop-basic.interface.ts
@@ -0,0 +1,6 @@
+export interface IShopBasic {
+    id: string;
+    name: string;
+    cashier_key_hash: string;
+    stamp_threshold: number;
+}
--- src/modules/shop/shop.service.ts
+++ src/modules/shop/shop.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { CoffeeShop } from "./entities/coffee-shop.entity";
+import { IShopBasic } from "./interfaces/shop-basic.interface";
 import { IShopSummary } from "./interfaces/shop-summary.interface";
 import { UpdateShopDto } from "./dto/update-shop.dto";
 
@@ -25,17 +26,16 @@ export class ShopService {
         }));
     }
 
-    async findById(
-        shopId: string,
-    ): Promise<{ id: string; cashier_key_hash: string; stamp_threshold: number } | null> {
+    async findById(shopId: string): Promise<IShopBasic | null> {
         const shop = await this.coffeeShopModel.findByPk(shopId, {
-            attributes: ["id", "cashier_key_hash", "stamp_threshold"],
+            attributes: ["id", "name", "cashier_key_hash", "stamp_threshold"],
         });
 
         if (!shop) return null;
 
         return {
             id: shop.id,
+            name: shop.name,
             cashier_key_hash: shop.cashier_key_hash,
             stamp_threshold: shop.stamp_threshold,
         };
